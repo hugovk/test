2d867f83752b0e2fe73855a0631059cc93438b2c
hugovk <hugovk@users.noreply.github.com>
2014-04-30 11:34:05 +0300

    test

--- test.py
+++ test.py
@@ -3,3 +3,13 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageChops
 print(Image.PILLOW_VERSION)
+
+try:
+    # give Tk a chance to set up the environment, in case we're
+    # using an _imaging module linked against libtcl/libtk (use
+    # __import__ to hide this from naive packagers; we don't really
+    # depend on Tk unless ImageTk is used, and that module already
+    # imports Tkinter)
+    __import__("FixTk")
+except ImportError:
+    pass
