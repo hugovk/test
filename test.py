43f9560efc487b21c85b5fc109186c7037c032df
hugovk <hugovk@users.noreply.github.com>
2014-04-30 12:20:01 +0300

    test imports

--- test.py
+++ test.py
@@ -13,3 +13,34 @@ try:
     __import__("FixTk")
 except ImportError:
     pass
+
+
+try:
+    # If the _imaging C module is not present, you can still use
+    # the "open" function to identify files, but you cannot load
+    # them.  Note that other modules should not refer to _imaging
+    # directly; import Image and use the Image.core variable instead.
+    from PIL import _imaging as core
+
+except ImportError as v:
+
+    # Fail here anyway. Don't let people run with a mostly broken Pillow.
+    raise
+
+
+try:
+    import builtins
+except ImportError:
+    import __builtin__
+    builtins = __builtin__
+
+
+from PIL import ImageMode
+from PIL._binary import i8, o8
+from PIL._util import isPath, isStringType #, deferred_error
+
+import os, sys
+
+# type stuff
+import collections
+import numbers
