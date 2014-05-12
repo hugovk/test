85e5676efaa2f089cb3dc196d2cdcc9eb1383d6d
hugovk <hugovk@users.noreply.github.com>
2014-05-12 17:36:02 +0300

    Test text w,h

--- test.py
+++ test.py
@@ -1,3 +1,21 @@
-import warnings
-warnings.simplefilter('default')
-import coverage
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+font_path = "Tests/fonts/FreeMono.ttf"
+font_size=20
+
+ttf = ImageFont.truetype(font_path, font_size)
+
+txt = "Hello World!"
+w, h = ttf.getsize(txt)
+print(w, h)
+
+img = Image.new("RGB", (256, 64), "white")
+d = ImageDraw.Draw(img)
+d.text((10, 10), txt, font=ttf, fill='black')
+
+img.show()
+img.save('font.png')
+
