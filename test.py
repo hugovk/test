8efb2d9b1077b7581c9ebbdf5902a4968fbb46a6
hugovk <hugovk@users.noreply.github.com>
2014-05-15 11:48:50 +0300

    test Pillow #663

--- test.py
+++ test.py
@@ -1,21 +1,7 @@
+#!/usr/bin/env python
+import sys
+import PySide
+sys.modules['PyQt4'] = PySide
 
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
-
-font_path = "Tests/fonts/FreeMono.ttf"
-font_size=20
-
-ttf = ImageFont.truetype(font_path, font_size)
-
-txt = "Hello World!"
-w, h = ttf.getsize(txt)
-print(w, h)
-
-img = Image.new("RGB", (256, 64), "white")
-d = ImageDraw.Draw(img)
-d.text((10, 10), txt, font=ttf, fill='black')
-
-img.show()
-img.save('font.png')
-
+import PySide.QtGui
+import PIL.ImageQt
