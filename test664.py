93b1f8ef2b39881c0199eef798f339abd76ab639
hugovk <hugovk@users.noreply.github.com>
2014-05-16 10:55:03 +0300

    Update test

--- test664.py
+++ test664.py
@@ -1,8 +1,11 @@
 from PIL import Image
 im = Image.open('test2.gif')
-im.save('somefile.png', 'PNG')
-im.thumbnail((10, 10))
-
-im2 = Image.open('somefile.png')
-im2.thumbnail((10, 10))
-im2.save('somefile2.png', 'PNG')
+im.save('testfile.png', 'PNG')
+im2 = Image.open('testfile.png')
+print im2.info
+# {'transparency': 255}
+im3 = im2.convert('RGBA')
+print im3
+# <PIL.Image.Image image mode=RGBA size=10x9 at 0x10C014B00>
+print im3.info
+# {'transparency': 255}
