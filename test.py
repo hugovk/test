22892234f3233bd080f72b72c86d0741c473820d
hugovk <hugovk@users.noreply.github.com>
2014-05-28 09:30:47 +0300

    Fix indent

--- test.py
+++ test.py
@@ -2,14 +2,14 @@
 
 from PIL import Image
 
-    im = Image.open("lena.ppm")
+im = Image.open("lena.ppm")
 
-    im.point(list(range(256))*3)
-    im.point(lambda x: x)
+im.point(list(range(256))*3)
+im.point(lambda x: x)
 
-    im = im.convert("I")
-    im.point(lambda x: x*1)
-    im.point(lambda x: x+1)
-    im.point(lambda x: x*1+1)
+im = im.convert("I")
+im.point(lambda x: x*1)
+im.point(lambda x: x+1)
+im.point(lambda x: x*1+1)
 
-    im.point(list(range(256))*256, 'L')
+im.point(list(range(256))*256, 'L')
