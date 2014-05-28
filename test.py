521f91ebcde31da51536023b0cc961ce5d0613cd
Hugo <hugovk@users.noreply.github.com>
2014-05-28 12:17:23 +0300

    Update test.py

--- test.py
+++ test.py
@@ -4,4 +4,12 @@ from PIL import Image
 
 im = Image.open("lena.ppm").convert("I")
 
+im.point(list(range(256))*3)
+im.point(lambda x: x)
+
+im = im.convert("I")
+im.point(lambda x: x*1)
+im.point(lambda x: x+1)
+im.point(lambda x: x*1+1)
+
 im.point(list(range(256))*256, 'L')
