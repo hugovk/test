141197e4133de1c73cec7126c5d29c87fd2064b1
hugovk <hugovk@users.noreply.github.com>
2014-06-26 22:45:45 +0300

    Fix print

--- test.py
+++ test.py
@@ -6,11 +6,11 @@ try:
     print("PyQt5 imported!")
 except Exception as e:
     print("PyQt5 not imported")
-    print str(e)
+    print(str(e))
     try:
         print("Try to import PyQt4...")
         from PyQt4.QtGui import QImage, qRgb, qRgba
         print("PyQt4 imported!")
     except Exception as e:
         print("PyQt4 not imported")
-        print str(e)
+        print(str(e))
