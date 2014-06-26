f9376419560bc0765a1f9de0159e98ce68758514
hugovk <hugovk@users.noreply.github.com>
2014-06-26 22:32:25 +0300

    Test pyqt on Travis

--- test.py
+++ test.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 
-from PIL import Image
-
-im = Image.open("lena.ppm")
-
-im.point(list(range(256))*3)
-im.point(lambda x: x)
-
-im = im.convert("I")
-im.point(lambda x: x*1)
-im.point(lambda x: x+1)
-im.point(lambda x: x*1+1)
-
-im.point(list(range(256))*256, 'L')
+try:
+    print("Try to import PyQt5...")
+    from PyQt5.QtGui import QImage, qRgb, qRgba
+    print("PyQt5 imported!")
+except Exception as e:
+        print("PyQt5 not imported")
+        print str(e)
+    try:
+        print("Try to import PyQt4...")
+        from PyQt4.QtGui import QImage, qRgb, qRgba
+        print("PyQt4 imported!")
+    except Exception as e:
+        print("PyQt4 not imported")
+        print str(e)
