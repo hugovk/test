56c8d4bdda0f8f84efc8139dc429237e5dea1c63
Hugo van Kemenade <hugovk@users.noreply.github.com>
2015-10-06 14:29:36 +0300

    Use im.size as im.height is newish

--- test.py
+++ test.py
@@ -3,6 +3,6 @@
 from django.core.files.images import ImageFile
 path = 'test-placeholder.png'
 im = ImageFile(open(path, 'rb'))
-print(im.height)
-assert(im.height == 22)
+print(im.size)
+assert(im.size[1] == 22)
 
