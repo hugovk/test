88cbeec037de65c007adf404b8272bad390a1fc8
Hugo van Kemenade <hugovk@users.noreply.github.com>
2022-12-13 10:55:17 +0200

    Install lxml

--- test.py
+++ test.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
 import sys
 
-from tqdm import tqdm
+import lxml
 
 print(123)
 
 print(sys.version)
 
-for i in tqdm(range(1000)):
-    pass
-
-print("My PIN is 1234")
+print(lxml.__version__)
