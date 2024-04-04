8f8368c7a7c51fc7c8d0d97c4dbf0a5cd0ad24cc
Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>
2024-04-04 13:22:00 +0300

    datetime.datetime.fromisoformat('0000W25')

--- test.py
+++ test.py
@@ -1,13 +1,4 @@
-#!/usr/bin/env python
-import sys
+import datetime
 
-from tqdm import tqdm
-
-print(123)
-
-print(sys.version)
-
-for i in tqdm(range(1000)):
-    pass
-
-print("My PIN is 1234")
+y = datetime.datetime.fromisoformat("0000W25")
+print(y)
