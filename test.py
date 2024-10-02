0cb2aa45886b20f0754c3170aeb9a6aa9ac73579
Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>
2024-10-02 17:06:28 +0300

    Test python-flint

--- test.py
+++ test.py
@@ -1,13 +1,4 @@
-#!/usr/bin/env python
-import sys
+from flint import *
 
-from tqdm import tqdm
-
-print(123)
-
-print(sys.version)
-
-for i in tqdm(range(1000)):
-    pass
-
-print("My PIN is 1234")
+print(fmpz(1000).partitions_p())
+print(fmpq.bernoulli(64))
