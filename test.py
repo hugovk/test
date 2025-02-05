8b263cafb69cd864c20fba7a1a4bf673af64bc0e
Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>
2025-02-05 22:57:22 +0200

    mimetypes.guess_file_type("my.xml")

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import mimetypes
 import sys
 
 from tqdm import tqdm
@@ -12,3 +13,5 @@ for i in tqdm(range(1000)):
     pass
 
 print("My PIN is 1234")
+
+print(mimetypes.guess_file_type("my.xml"))
